29d4d56e98344232ea10fe473978aee8e08df83a
Abhinav Verma <abhinavverma658@gmail.com>
2025-10-30 15:32:35 +0530

    fixing contact us

--- src/pages/other-pages/contact/components/Contact.jsx
+++ src/pages/other-pages/contact/components/Contact.jsx
@@ -163,7 +163,7 @@ const Contact = () => {
                 <Row className=" flex-lg-row flex-column-reverse">
                     <Col lg={6} className="mb-30">
                         <div className="vl-maps">
-                            <iframe src={contactPageData?.googleMapsEmbedLink || "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d423284.04421055503!2d-118.74139674995793!3d34.020608447020685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c75ddc27da13%3A0xe22fdf6f254608f4!2sLos%20Angeles%2C%20CA%2C%20USA!5e0!3m2!1sen!2sbd!4v1734253501055!5m2!1sen!2sbd"} allowFullScreen className="vl-contact-maps" />
+                            <iframe src={contactPageData?.googleMapsEmbedLink || "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d423284.04421055503!2d-118.74139674995793!3d34.020608447020685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c75ddc27da13%3A0xe22fdf6f254608f4!2sLos%20Angeles%2C%20CA%2C%20USA!5e0!3m2!1sen!2sbd!4v1734253501055!5m2!1sen!2sbd"} allowFullScreen className="vl-contact-maps" style={{ height: "800px" }} />
                         </div>
                     </Col>
                     <Col lg={6} className="mb-30">
